bac0575f4ab5b4304bf766fb1d59a324d14859d6
Jacob Gasser <43097555+jacobgasser@users.noreply.github.com>
2019-12-15 18:53:38 -0500

    Added sanitation to user input

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin')
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const forbiddenCharacters = ['<', '>', '&lt;', '&gt;', '&']
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -18,12 +19,39 @@ app.use(cors())
 
 app.use(express.urlencoded())
 
-app.post('/createuser', async (req, res) => {
-    db.collection('players').add(req.body).then(ref => {
+app.post('/create/player', async (req, res) => {
+    const player = req.body
+    let sanitizedPlayer = sanitizeUser(player)
+    if (typeof (sanitizedPlayer) === String) {
+        return res.send(sanitizedPlayer)
+    }
+
+    db.collection('players').add(sanitizedPlayer).then(ref => {
         return res.send("success")
-    }).catch(console.log)
+    }).catch(err => {
+        console.log(err)
+        return res.send("Error: Could not create player")
+    })
 })
 
 
+function sanitizeUser(uncleansed) {
+    for (i of JSON.stringify(uncleansed).split("")) {
+        if (forbiddenCharacters.includes(i)) {
+            return "Error: Inputs cannot contain any of the forbidden characters!"
+        }
+    }
+    const cleansed = {
+        name: uncleansed.name,
+        highschool: uncleansed.highschool,
+        height: parseInt(uncleansed.height),
+        weight: parseInt(uncleansed.weight),
+        mainPosition: uncleansed.mainPosition,
+        sidePosition: uncleansed.sidePosition,
+        arm: uncleansed.arm,
+        graduatingClass: parseInt(uncleansed.graduatingClass),
+    }
 
+    return cleansed
+}
 exports.api = functions.https.onRequest(app)
