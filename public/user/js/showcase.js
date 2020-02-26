94ff13ce2ea257355d52cad09f14b9297db727b6
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2020-02-25 21:19:39 -0500

    Fast

--- public/user/js/showcase.js
+++ public/user/js/showcase.js
@@ -1,15 +1,14 @@
 document.querySelectorAll(".attBtn").forEach(function(btn) {
   btn.onclick = (evt) => {
-    const e = evt || window.event;
-    const parent = e.target.parentNode || e.srcElement.parentNode;
-    const target = e.target || e.srcElement;
+    const e = evt || window.event
+    const parent = e.target.parentNode || e.srcElement.parentNode
+    const target = e.target || e.srcElement
 
-    if (parent.querySelector(".active")) parent.querySelector(".active").classList.remove("active");
+    if (parent.querySelector(".active")) parent.querySelector(".active").classList.remove("active")
 
-    target.classList.toggle("active");
-  };
-});
-
+    target.classList.toggle("active")
+  }
+})
 //Auth System
 var firebaseConfig = {
   apiKey: "AIzaSyDOjiZ--U5gWKZTMyvd3juQle-4OQuHGpE",
@@ -20,23 +19,23 @@ var firebaseConfig = {
   messagingSenderId: "376398836043",
   appId: "1:376398836043:web:abbd1bb48d5dc1b1b538ce",
   measurementId: "G-044W9K81T1"
-};
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+}
+firebase.initializeApp(firebaseConfig)
+const auth = firebase.auth()
 //firebase.auth().onAuthStateChanged(function(user) {
-if (user) {
-  alert(`Welcome, ${user.email}`);
+if ( user ) {
+  alert(`Welcome, ${user.email}`)
 } else {
-  alert("You need to login first!");
-  window.location = "https://go-scout.web.app/index";
+  alert("You need to login first!")
+  window.location = "https://go-scout.web.app/index"
 }
 //})
 //logout
-const logout = document.querySelector("#logout");
+const logout = document.querySelector("#logout")
 logout.addEventListener("click", (e) => {
-  e.preventDefault();
+  e.preventDefault()
   auth.signOut().then(() => {
-    console.log("User Logged Out");
-  });
-  location.assign("https://go-scout.web.app");
-});
+    console.log("User Logged Out")
+  })
+  location.assign("https://goscoutllc.com/")
+})
